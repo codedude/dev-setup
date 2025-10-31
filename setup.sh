e5f74b79bc4402405a7ea3eb46856c2c1981d8c6
vparis <vparis@akamai.com>
2025-10-31 19:28:16 +0000

    update

--- setup.sh
+++ setup.sh
@@ -2,19 +2,27 @@
 
 # Install deps
 apt update
-apt install -y neovim git curl wget fzf ripgrep thefuck lsd fd-find tree-sitter nodejs tmux lazygit tldr luarocks
+apt install -y neovim git curl wget fzf ripgrep thefuck lsd fd-find nodejs npm tmux tldr luarocks
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
+
+npm install -g tree-sitter-cli
 
 # Bash config
 touch ~/.bashrc
-echo "alias bashconfig=nvim ~/.bashrc\nalias bashreload=source ~/.bashrc" >>~/.bashrc
-eval $(thefuck --alias)
+echo -e "alias bashconfig=\"nvim ~/.bashrc\"\nalias bashreload=\"source ~/.bashrc\"" >> ~/.bashrc
+echo 'eval $(thefuck --alias)' >> ~/.bashrc
 
 # tmux conf
+touch ~/.tmux.conf
 cp ~/.tmux.conf ~/.tmux.conf.OLD
 cp .tmux.conf ~/.tmux.conf
 
 # nvim conf
-echo "alias vi=nvim\nalias vim=nvim\nEDITOR=nvim" >>~/.bashrc
+echo -e "alias vi=nvim\nalias vim=nvim\nEDITOR=nvim" >> ~/.bashrc
 cp -r nvim ~/.config/nvim
 cd ~/.config/nvim
 
